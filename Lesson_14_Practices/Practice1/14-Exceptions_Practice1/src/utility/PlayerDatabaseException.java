d0f9629d340bb28a5d8c995ea914a1231f817d7c
Anjali <anjali.payal69@gmail.com>
2020-08-17 18:24:44 +0200

    Adding Exception Handling

---
+++ Lesson_14_Practices/Practice1/14-Exceptions_Practice1/src/utility/PlayerDatabaseException.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utility;
+
+/**
+ *
+ * @author anjali
+ */
+public class PlayerDatabaseException extends Exception {
+    
+    public PlayerDatabaseException() {}
+    
+    public PlayerDatabaseException(String message) {
+        super(message);
+    }
+    
+    
+}
